6560d5fac4550326e5555e07289cef153cfa6c9d
rehmankhan786 <arkhan.rock473@gmail.com>
2025-02-06 21:33:40 +0530

    CORS updated

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 4000;
 const origin1 = process.env.FRONTEND
 // const origin2 = "https://vh0820sq-4000.inc1.devtunnels.ms/"
 // console.log(origin)
-app.use(cors());
+app.use(cors({
+  origin: origin1,
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
