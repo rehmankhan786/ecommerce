99d3bd1d7d436741ca241818a0b13dd6753b4064
rehmankhan786 <arkhan.rock473@gmail.com>
2025-02-06 21:07:33 +0530

    updated coed policies

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ require("./db/connection.js");
 const cookieParser = require("cookie-parser");
 const mainRouter = require("./routes/mainRouter.js");
 const cors = require("cors");
+var ghpages = require('gh-pages');
 
+// ghpages.publish('dist', function(err) {});
 const app = express();
 const port = process.env.PORT || 4000;
-const origin1 = "http://localhost:3000"
+const origin1 = process.env.FRONTEND
 // const origin2 = "https://vh0820sq-4000.inc1.devtunnels.ms/"
 // console.log(origin)
-app.use(cors({ origin: origin1, credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
